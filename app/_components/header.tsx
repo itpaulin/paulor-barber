6b0b0ff269de6ecb124005191aa6f451310ca8cc
Paulo Ricardo <prmaltaleal@gmail.com>
2024-01-30 03:13:23 -0300

    style: change size of sheets

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Card } from "./ui/card";
+import { Card, CardContent } from "./ui/card";
 import {
   Sheet,
   SheetClose,
@@ -17,74 +17,78 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <Card className="flex h-[90px] w-full flex-row items-center justify-between px-5 py-6">
-      <Link href="/">
-        <Image
-          src="/logo.png"
-          alt="logo PauloR Barber"
-          width={130}
-          height={0}
-          className=""
-        />
-      </Link>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="ghost">
-            <Menu size={20} />
-          </Button>
-        </SheetTrigger>
-        <SheetContent>
-          <div className="mb-7 flex justify-start">
-            <h1 className="font-semibold">Menu</h1>
-          </div>
-          <div className="absolute right-0 w-full border border-accent"></div>
-          <div className="mb-6">
-            <div className=" flex flex-row pb-3 pt-6">
-              <Avatar>
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <span className="px-3 pt-2 font-normal">
-                Olá. Faça seu login!
-              </span>
-            </div>
-            <div>
-              <Button
-                variant="outline"
-                className="flex w-full items-center  justify-start gap-x-2 rounded-xl"
-              >
-                <LogIn size={16} />
-                Fazer Login
+    <header>
+      <Card>
+        <CardContent className="flex h-[90px] w-full flex-row items-center justify-between px-5 py-6">
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              alt="logo PauloR Barber"
+              width={130}
+              height={0}
+              className=""
+            />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon" className="h-8 w-8">
+                <Menu size={20} />
               </Button>
-            </div>
-          </div>
+            </SheetTrigger>
+            <SheetContent>
+              <div className="mb-7 flex justify-start">
+                <h1 className="font-semibold">Menu</h1>
+              </div>
+              <div className="absolute right-0 w-full border border-accent"></div>
+              <div className="mb-6">
+                <div className=" flex flex-row pb-3 pt-6">
+                  <Avatar>
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <span className="px-3 pt-2 font-normal">
+                    Olá. Faça seu login!
+                  </span>
+                </div>
+                <div>
+                  <Button
+                    variant="outline"
+                    className="flex w-full items-center  justify-start gap-x-2 rounded-xl"
+                  >
+                    <LogIn size={16} />
+                    Fazer Login
+                  </Button>
+                </div>
+              </div>
 
-          <div className=" flex w-full flex-col gap-y-3">
-            <Link href="/">
-              <Button
-                variant="outline"
-                className="flex w-full items-center  justify-start gap-x-2 rounded-xl"
-              >
-                <Home size={16} />
-                Início
-              </Button>
-            </Link>
-            <Link href="/">
-              <Button
-                variant="outline"
-                className="flex w-full items-center justify-start gap-x-2 rounded-xl"
-              >
-                <CalendarDaysIcon size={16} />
-                Agendamentos
-              </Button>
-            </Link>
-          </div>
-        </SheetContent>
-      </Sheet>
-    </Card>
+              <div className=" flex w-full flex-col gap-y-3">
+                <Link href="/">
+                  <Button
+                    variant="outline"
+                    className="flex w-full items-center  justify-start gap-x-2 rounded-xl"
+                  >
+                    <Home size={16} />
+                    Início
+                  </Button>
+                </Link>
+                <Link href="/">
+                  <Button
+                    variant="outline"
+                    className="flex w-full items-center justify-start gap-x-2 rounded-xl"
+                  >
+                    <CalendarDaysIcon size={16} />
+                    Agendamentos
+                  </Button>
+                </Link>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
   );
 };
 
