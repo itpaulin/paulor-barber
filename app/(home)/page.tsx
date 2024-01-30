02d22c981c58070e5f728cd7645321763e4cf906
Paulo Ricardo <prmaltaleal@gmail.com>
2024-01-30 03:32:53 -0300

    feat: add home page title

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
-    <h1>oi</h1>
+    <div className="flex flex-col gap-y-1 px-5 py-6">
+      <p className="text-xl">
+        Olá, <span className="font-semibold">Paulo</span>!
+      </p>
+      <p className="text-sm capitalize">
+        {format(new Date(), "EEEE ',' d 'de ' LLLL", { locale: ptBR })}
+      </p>
+    </div>
   );
 }
