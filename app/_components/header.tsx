77740b390d8b9f5b8d41901b8a1219eb80e8d23e
Paulo Ricardo <prmaltaleal@gmail.com>
2024-01-31 23:45:07 -0300

    feat: ensure show the name of user and button logout

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import {
@@ -11,11 +12,26 @@ import {
   SheetTrigger,
 } from "./ui/sheet";
 import { Button } from "./ui/button";
-import { Calendar, CalendarDaysIcon, Home, LogIn, Menu } from "lucide-react";
+import {
+  Calendar,
+  CalendarDaysIcon,
+  Home,
+  LogIn,
+  LogOut,
+  Menu,
+} from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data } = useSession();
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
   return (
     <Card className="rounded-none bg-[#141518]">
       <CardContent className="flex h-[90px] w-full flex-row items-center justify-between px-5 py-6">
@@ -40,26 +56,44 @@ const Header = () => {
             </div>
             <div className="absolute right-0 w-full border border-accent"></div>
             <div className="mb-6">
-              <div className=" flex flex-row pb-3 pt-6">
-                <Avatar>
-                  <AvatarImage
-                    src="https://github.com/shadcn.png"
-                    alt="@shadcn"
-                  />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <span className="px-3 pt-2 font-normal">
-                  Olá. Faça seu login!
-                </span>
-              </div>
+              {data?.user ? (
+                <div className=" flex flex-row items-center pt-6">
+                  <Avatar>
+                    <AvatarImage src={data.user.image!} alt="@shadcn" />
+                    <AvatarFallback>{data.user.name}</AvatarFallback>
+                  </Avatar>
+                  <span className="px-3 pt-1 font-normal">
+                    {data.user.name}
+                  </span>
+                  <Button variant="outline" size="icon" className="pt-1">
+                    <LogOut size={20} />
+                  </Button>
+                </div>
+              ) : (
+                <div className=" flex flex-row pb-3 pt-6">
+                  <Avatar>
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <span className="px-3 pt-2 font-normal">
+                    Olá. Faça seu login!
+                  </span>
+                </div>
+              )}
               <div>
-                <Button
-                  variant="outline"
-                  className="flex w-full items-center  justify-start gap-x-2 rounded-xl"
-                >
-                  <LogIn size={16} />
-                  Fazer Login
-                </Button>
+                {!data?.user && (
+                  <Button
+                    variant="outline"
+                    className="flex w-full items-center  justify-start gap-x-2 rounded-xl"
+                    onClick={handleLoginClick}
+                  >
+                    <LogIn size={16} />
+                    Fazer Login
+                  </Button>
+                )}
               </div>
             </div>
 
@@ -77,6 +111,7 @@ const Header = () => {
                 <Button
                   variant="outline"
                   className="flex w-full items-center justify-start gap-x-2 rounded-xl"
+                  disabled={!data?.user}
                 >
                   <CalendarDaysIcon size={16} />
                   Agendamentos
