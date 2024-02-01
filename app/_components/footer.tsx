b5f3542fce72da47ba9c1bc4b5ca4ce617762c0b
Paulo Ricardo <prmaltaleal@gmail.com>
2024-01-31 22:46:40 -0300

    feat: add footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="mt-8 bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+      © 2023 Copyright{" "}
+      <span className="font-semibold">© 2024 Copyright PauloR Barber</span>
+    </div>
+  );
+};
+
+export default Footer;
