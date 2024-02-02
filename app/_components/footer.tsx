ef0db96b11a5f2002a4bd4d491a3606644e7cb72
Paulo Ricardo <prmaltaleal@gmail.com>
2024-02-02 00:51:51 -0300

    fix: ensure text footer is correct

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <div className="mt-8 bg-accent px-8 py-4 text-[0.625rem] opacity-75">
-      © 2023 Copyright{" "}
-      <span className="font-semibold">© 2024 Copyright PauloR Barber</span>
+      © 2024 Copyright&nbsp;
+      <span className="font-semibold">PauloR Barber</span>
     </div>
   );
 };
