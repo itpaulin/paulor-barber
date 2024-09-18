fb8e6d37c85fc7946c167d893b974c56589b43fb
Paulo Ricardo <prmaltaleal@gmail.com>
2024-09-18 01:38:06 -0300

    feat: update props and show description barbershop

--- app/barbershop/[id]/page.tsx
+++ app/barbershop/[id]/page.tsx
@@ -15,8 +15,7 @@ interface BarbershopDetailsPageProps {
 const BarbershopDetailsPage = async ({
   params,
 }: BarbershopDetailsPageProps) => {
-  const session =  await getServerSession(authOptions)
-  
+  const session = await getServerSession(authOptions);
 
   const barbershop = await db.barbershop.findUnique({
     where: {
@@ -35,15 +34,29 @@ const BarbershopDetailsPage = async ({
       <BarbershopInfo barbershop={barbershop} />
 
       <Tabs
-        infoContent={<div>
-           <h2 className=" mb-3 text-xs font-bold uppercase text-gray-400">sobre nós</h2>
-           <p className="text-sm font-light">Bem-vindo à Vintage Barber, onde tradição encontra estilo. Nossa equipe de mestres barbeiros transforma cortes de cabelo e barbas em obras de arte. Em um ambiente acolhedor, promovemos confiança, estilo e uma comunidade unida.</p>
-           <div className="flex-grow border-t border-gray-300" />
-        </div>}
+        infoContent={
+          <div>
+            <h2 className=" mb-3 text-xs font-bold uppercase text-gray-400">
+              sobre nós
+            </h2>
+            <p className="text-sm font-light">
+              Bem-vindo à Vintage Barber, onde tradição encontra estilo. Nossa
+              equipe de mestres barbeiros transforma cortes de cabelo e barbas
+              em obras de arte. Em um ambiente acolhedor, promovemos confiança,
+              estilo e uma comunidade unida.
+            </p>
+            <div className="flex-grow border-t border-gray-300" />
+          </div>
+        }
         servicesContent={
           <div className="grid grid-cols-1 gap-y-3">
             {barbershop.services.map((service) => (
-              <ServiceItem service={service} key={service.id} isAuthenticated={!!session?.user}/>
+              <ServiceItem
+                barbershop={barbershop}
+                service={service}
+                key={service.id}
+                isAuthenticated={!!session?.user}
+              />
             ))}
           </div>
         }
