f65705c16cdce0b4ca563f2db3371a594a267296
Paulo Ricardo <prmaltaleal@gmail.com>
2024-09-21 18:21:43 -0300

    feat: change font project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
-import Header from "./_components/header";
 import Footer from "./_components/footer";
 import AuthProvider from "./_providers/auth";
 import { Toaster } from "./_components/ui/sonner";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} dark`}>
+      <body className={`${nunito.className} dark`}>
         <AuthProvider>
           <div className="flex-1">{children}</div>
           <Toaster />
