4947d37104f7104140c2b7f0a33c73ca910c3aff
Paulo Ricardo <prmaltaleal@gmail.com>
2024-09-21 18:22:32 -0300

    feat: ensure show cards booking according to resposne

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,34 +7,60 @@ import { db as prismaClient } from "@/app/_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
 import Hello from "./_components/hello";
 import Header from "../_components/header";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Home() {
-  const barbershops = await prismaClient.barbershop.findMany();
+  const session = await getServerSession(authOptions);
+
+  const [barbershops, confirmedBookings] = await Promise.all([
+    prismaClient.barbershop.findMany({}),
+
+    session?.user
+      ? prismaClient.booking.findMany({
+          where: {
+            date: {
+              gte: new Date(),
+            },
+          },
+          include: {
+            service: true,
+            barbershop: true,
+          },
+        })
+      : Promise.resolve([]),
+  ]);
 
   return (
     <div className="">
       <Header />
-      <div className="px-5">
-        <div className="flex flex-col gap-y-1  py-6">
+      <div className="">
+        <div className="flex flex-col gap-y-1  px-5 py-6">
           <Hello />
           <p className="text-sm capitalize">
             {format(new Date(), "EEEE ',' d 'de ' LLLL", { locale: ptBR })}
           </p>
         </div>
-        <div className="w-full">
+        <div className="w-full px-5">
           <Search />
         </div>
-        <div className="mt-9 flex flex-col gap-y-3">
-          <h2 className=" text-xs font-bold uppercase text-gray-400">
-            Agendamentos
-          </h2>
-          <BookingItem />
-        </div>
+        {confirmedBookings.length > 0 && (
+          <div className="mt-9 flex flex-col gap-y-3">
+            <h2 className=" px-5 text-xs font-bold uppercase text-gray-400">
+              Agendamentos
+            </h2>
+            <div className="no-scrollbar flex gap-3 overflow-x-auto overflow-y-hidden px-5">
+              {confirmedBookings.map((booking, i) => (
+                <BookingItem key={i} booking={booking} />
+              ))}
+            </div>
+          </div>
+        )}
         <div className="mt-6">
-          <h2 className=" mb-3 text-xs font-bold uppercase text-gray-400">
+          <h2 className=" mb-3 px-5 text-xs font-bold uppercase text-gray-400">
             Recomendados
           </h2>
-          <div className="no-scrollbar flex gap-4 overflow-x-auto">
+          <div className="no-scrollbar flex gap-4 overflow-x-auto px-5">
             {barbershops
               .map((barbershop, index) => (
                 <div className="w-[167px]" key={index}>
@@ -45,10 +71,10 @@ export default async function Home() {
           </div>
         </div>
         <div className="mt-6">
-          <h2 className=" mb-3 text-xs font-bold uppercase text-gray-400">
+          <h2 className=" mb-3 px-5 text-xs font-bold uppercase text-gray-400">
             Populares
           </h2>
-          <div className="no-scrollbar flex gap-4 overflow-x-auto">
+          <div className="no-scrollbar flex gap-4 overflow-x-auto px-5">
             {barbershops
               .map((barbershop, index) => (
                 <div className="w-[167px]" key={index}>
