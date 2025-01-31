bfa37052b4a5cea8c8ad5a979a1ec9398353e640
Paulo Ricardo Malta <prmaltaleal@gmail.com>
2025-01-30 23:43:46 -0300

    feat: add open hours

--- app/barbershop/[id]/page.tsx
+++ app/barbershop/[id]/page.tsx
@@ -33,7 +33,9 @@ const BarbershopDetailsPage = async ({
   return (
     <div className="">
       <BarbershopInfo barbershop={barbershop} />
-      <div className="mt-6 flex-grow border-separate border-t" />
+
+      <Divider className="mt-6" />
+
       <BarbershopTabDetails
         servicesContent={
           <div className="grid grid-cols-1 gap-y-3 px-5">
@@ -52,17 +54,57 @@ const BarbershopDetailsPage = async ({
             <h2 className=" mb-3 px-5 text-xs font-bold uppercase text-gray-400">
               sobre nós
             </h2>
+
+            {/* //TODO make dinamic with DB */}
+
             <p className="px-5 text-sm font-light">
               Bem-vindo à Vintage Barber, onde tradição encontra estilo. Nossa
               equipe de mestres barbeiros transforma cortes de cabelo e barbas
               em obras de arte. Em um ambiente acolhedor, promovemos confiança,
               estilo e uma comunidade unida.
             </p>
+
             <Divider className="my-6" />
+
+            {/* //TODO make dinamic with DB */}
 
             <div className="flex flex-col gap-3 px-5">
               <BarbershopContactPhone phoneNumber="(22) 98805-5432" />
               <BarbershopContactPhone phoneNumber="(22) 93772-2819" />
+            </div>
+
+            <Divider className="my-6" />
+
+            {/* //TODO make dinamic with DB */}
+            <div className="flex flex-col gap-2 px-5">
+              <div className="flex flex-row justify-between">
+                <p className="font-semibold text-gray-500">Segunda-Feira</p>
+                <p className="font-semibold">Fechado</p>
+              </div>
+              <div className="flex flex-row justify-between">
+                <p className="font-semibold text-gray-500">Terça-Feira</p>
+                <p className="font-semibold">09:00 - 21:00</p>
+              </div>
+              <div className="flex flex-row justify-between">
+                <p className="font-semibold text-gray-500">Quarta-Feira</p>
+                <p className="font-semibold">09:00 - 21:00</p>
+              </div>
+              <div className="flex flex-row justify-between">
+                <p className="font-semibold text-gray-500">Quinta-Feira</p>
+                <p className="font-semibold">09:00 - 21:00</p>
+              </div>
+              <div className="flex flex-row justify-between">
+                <p className="font-semibold text-gray-500">Sexta-Feira</p>
+                <p className="font-semibold">09:00 - 21:00</p>
+              </div>
+              <div className="flex flex-row justify-between">
+                <p className="font-semibold text-gray-500">Sábado</p>
+                <p className="font-semibold">08:00 - 17:00</p>
+              </div>
+              <div className="flex flex-row justify-between">
+                <p className="font-semibold text-gray-500">Domingo</p>
+                <p className="font-semibold">Fechado</p>
+              </div>
             </div>
           </div>
         }
