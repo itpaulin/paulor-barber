a49e81b1730a1339b1243e8e3ef5f4e0b1bd4b69
Paulo Ricardo Malta <prmaltaleal@gmail.com>
2025-01-30 23:10:57 -0300

    feat: add barbershop contact phone component

---
+++ app/barbershop/[id]/_components/barbershop-contact-phone.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { Copy, Smartphone } from "lucide-react";
+import { useEffect, useState } from "react";
+
+const BarbershopContactPhone = ({ phoneNumber }: { phoneNumber: string }) => {
+  const [copiedText, setCopiedText] = useState("Copiar");
+
+  const checkNumberIsCopied = async () => {
+    try {
+      const clipboard = await navigator.clipboard.readText();
+      return clipboard === phoneNumber;
+    } catch {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    const verifyClipboard = async () => {
+      const numberIsCopied = await checkNumberIsCopied();
+      setCopiedText(numberIsCopied ? "Copiado!" : "Copiar");
+    };
+
+    verifyClipboard();
+  }, [phoneNumber]); // Atualiza sempre que `phoneNumber` mudar
+
+  return (
+    <div className="flex flex-row items-center justify-between gap-2">
+      <div className="flex flex-row items-center gap-2">
+        <Smartphone size={22} className="text-white" />
+        <p>{phoneNumber}</p>
+      </div>
+      <Button
+        onClick={async () => {
+          await navigator.clipboard.writeText(phoneNumber);
+          setCopiedText("Copiado!");
+        }}
+        size="sm"
+        variant={copiedText === "Copiado!" ? "default" : "secondary"}
+        className="rounded-lg"
+      >
+        {copiedText}
+      </Button>
+    </div>
+  );
+};
+
+export default BarbershopContactPhone;
